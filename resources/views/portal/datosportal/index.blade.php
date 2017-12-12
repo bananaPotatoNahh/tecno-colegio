08750c7367010c115ff7d01e18e81770cd2ca3a5
Mauricio Ballesteros <mballes95@gmail.com>
2017-12-12 04:50:07 -0400

    Renaming Template for use in all Views

--- resources/views/portal/datosportal/index.blade.php
+++ resources/views/portal/datosportal/index.blade.php
@@ -3,7 +3,7 @@
     <div class="row">
 
         <div class="col-lg-8 col-md-8 col-sm-8 col-xs-12">
-            <h3>Listado de Portal <a href="create"><button  class="btn btn-bitbucket">Nuevo</button></a> </h3>
+            <h3>Listado de Portal <a href="datosportal/create"><button  class="btn btn-bitbucket">Nuevo</button></a> </h3>
             @include('portal.datosportal.search')
 
         </div>
