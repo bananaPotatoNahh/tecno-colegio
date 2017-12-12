f7fd5cdafce5a57c23751f7a42c79ee9ad83d5ec
Mauricio Ballesteros <mballes95@gmail.com>
2017-12-12 04:42:12 -0400

    Adding Index View

---
+++ resources/views/layouts/principal.blade.php
@@ -0,0 +1,69 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="icon" href="./favicon.ico">
+
+    <title>@yield("title","Acropolis")</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="{{asset('libraries/bootstrap/css/bootstrap.min.css')}}" rel="stylesheet">
+    <link href="{{asset('libraries/vis/css/vis.min.css')}}" rel="stylesheet">
+
+    @if($_COOKIE['tema'] == 2)
+        <link id="refEstilos" href="{{asset('assets/css/theme2.css')}}" rel="stylesheet">
+    @elseif($_COOKIE['tema'] == 3)
+        <link id="refEstilos" href="{{asset('assets/css/theme3.css')}}" rel="stylesheet">
+    @else
+        <link id="refEstilos" href="{{asset('assets/css/theme1.css')}}" rel="stylesheet">
+    @endif
+
+<!-- Custom styles for this template -->
+    <style>
+        body {
+            padding-top: 50px;
+            padding-bottom: 20px;
+        }
+    </style>
+
+</head>
+
+<body>
+
+@include("layouts.menu")
+
+<!-- Main jumbotron for a primary marketing message or call to action -->
+<div class="backgroud-back">
+    <div class="backgroud-top">
+        <div class="container">
+            @yield("contenido")
+        </div>
+    </div>
+</div>
+
+<!-- Bootstrap core JavaScript
+================================================== -->
+<!-- Placed at the end of the document so the pages load faster -->
+<script src="{{asset('libraries/jQuery/js/jquery-3.2.1.min.js')}}"></script>
+<script src="{{asset('libraries/js.cookie.js')}}"></script>
+<script src="{{asset('libraries/bootstrap/js/bootstrap.min.js')}}"></script>
+<script src="{{asset('libraries/vis/js/vis.min.js')}}"></script>
+<script type="application/javascript">
+    $('#tema1').click(function () {
+        Cookies.set('tema', 1);
+        location.reload();
+    });
+    $('#tema2').click(function () {
+        Cookies.set('tema', 2);
+        location.reload();
+    });
+    $('#tema3').click(function () {
+        Cookies.set('tema', 3);
+        location.reload();
+    });
+</script>
+@yield("scripts")
+</body>
+</html>
